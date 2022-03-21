5e6126ac1da67c93acda54d38134492a609d7ba8
SHARATH KUMAR B <sharath1981@gmail.com>
2022-03-21 10:34:57 +0530

    Refactored

--- src/main/java/com/ryana/config/DynamoDBConfig.java
+++ src/main/java/com/ryana/config/DynamoDBConfig.java
@@ -7,21 +7,26 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Configuration
 public class DynamoDBConfig {
 
-    @Value("${aws.dynamodb.endpoint}")
-    public String SERVICE_ENDPOINT;
-    @Value("${aws.dynamodb.region}")
-    public String REGION;
-    @Value("${aws.dynamodb.accessKey}")
-    public String ACCESS_KEY;
-    @Value("${aws.dynamodb.secretKey}")
-    public String SECRET_KEY;
+    private final Environment environment;
+
+    // @Value("${aws.dynamodb.endpoint}")
+    // public String SERVICE_ENDPOINT;
+    // @Value("${aws.dynamodb.region}")
+    // public String REGION;
+    // @Value("${aws.dynamodb.accessKey}")
+    // public String ACCESS_KEY;
+    // @Value("${aws.dynamodb.secretKey}")
+    // public String SECRET_KEY;
 
     @Bean
     public DynamoDBMapper mapper() {
@@ -29,9 +34,13 @@ public class DynamoDBConfig {
     }
 
     private AmazonDynamoDB amazonDynamoDBConfig() {
+        final var endpoint = environment.getProperty("aws.dynamodb.endpoint");
+        final var region = environment.getProperty("aws.dynamodb.region");
+        final var accessKey = environment.getProperty("aws.dynamodb.accessKey");
+        final var secretKey = environment.getProperty("aws.dynamodb.secretKey");
         return AmazonDynamoDBClientBuilder.standard()
-                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(SERVICE_ENDPOINT, REGION))
-                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(ACCESS_KEY, SECRET_KEY)))
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endpoint, region))
+                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey)))
                 .build();
     }
 }
